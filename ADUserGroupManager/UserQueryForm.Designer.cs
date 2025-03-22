dd4211f527a38b780f88cf86142f28f569b11c3a
Ezequiel Banega <ezequielbanega@gmail.com>
2025-03-21 22:58:10 -0300

    c

--- ADUserGroupManager/UserQueryForm.Designer.cs
+++ ADUserGroupManager/UserQueryForm.Designer.cs
@@ -105,7 +105,7 @@
             // 
             // btnUnlockUser
             // 
-            this.btnUnlockUser.BackColor = System.Drawing.Color.Maroon;
+            this.btnUnlockUser.BackColor = System.Drawing.Color.Black;
             this.btnUnlockUser.FlatAppearance.BorderSize = 0;
             this.btnUnlockUser.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnUnlockUser.ForeColor = System.Drawing.Color.White;
