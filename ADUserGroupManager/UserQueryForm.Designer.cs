ee6e4756cc5e679c2b4f93e7f16d83daee9327e1
Ezequiel Banega <ezequielbanega@gmail.com>
2025-03-21 21:38:04 -0300

    test btn color

--- ADUserGroupManager/UserQueryForm.Designer.cs
+++ ADUserGroupManager/UserQueryForm.Designer.cs
@@ -105,7 +105,7 @@
             // 
             // btnUnlockUser
             // 
-            this.btnUnlockUser.BackColor = System.Drawing.Color.Black;
+            this.btnUnlockUser.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
             this.btnUnlockUser.FlatAppearance.BorderSize = 0;
             this.btnUnlockUser.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnUnlockUser.ForeColor = System.Drawing.Color.White;
